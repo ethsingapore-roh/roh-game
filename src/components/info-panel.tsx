edf4f6e29a43bf30a9b88d0c7430cd163ead1310
Otto G <5030059+ottodevs@users.noreply.github.com>
2024-09-22 01:31:07 +0800

    chore: refactor and split components from onboarding scene

---
+++ src/components/info-panel.tsx
@@ -0,0 +1,21 @@
+import { AlertCircle } from "lucide-react";
+import type { GameState } from "@/types/game-state";
+
+export default function InfoPanel({gameState}: {gameState: GameState}) {
+  return (
+<div className="absolute top-6 left-6 w-[400px] h-[500px] bg-[url('/images/dashboard.svg')] bg-no-repeat bg-contain p-4 flex flex-col">
+        <p className="text-l relative bottom-[3px] text-center align-middle justify-center"> Level 1</p>
+        <div className="flex flex-col ml-[32px] mt-[10%]">
+          <h2 className="text-2xl mb-4  flex items-center"><AlertCircle className="mr-2" /> System Status</h2>
+          <p className="mb-2">Prisoner ID: #45721</p>
+          <p className="mb-2">Security Level: Maximum</p>
+          <p className="mb-2">AI Threat Level: Critical</p>
+          <p className="mb-2">Verification Status: {gameState.verified ? 'Confirmed Human' : 'Unverified'}</p>
+          <div className="mt-auto">
+            <p className="text-xs opacity-50">Marina Bay Correctional Facility</p>
+            <p className="text-xs opacity-50">Year 2049</p>
+          </div>
+        </div>
+      </div>
+  )
+}
---
+++ src/types/game-state.ts
@@ -0,0 +1,8 @@
+export type GameState = {
+    verified: boolean;
+    securityLevel: number;
+    aiThreatLevel: number;
+    prisonerId: string;
+    level: number;
+    dialogueStep: number;
+}
